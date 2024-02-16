490ebdb113d0ef956114e1b9428ab934ac30ad3a
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-16 14:30:41 +0000

    Second Commit Update

--- grading.js
+++ grading.js
@@ -1,10 +1,10 @@
 // Grading system function
 
 function gradingSystem(score){
-    if (score >= 90){
+    if (score >= 90 && score <=100){
         return "A"
     } 
 }
 
-const myGrade = gradingSystem(98)
-console.log(myGrade)
+const myGrade = gradingSystem(108)
+console.log(myGrade) // A
